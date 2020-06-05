36ab0a756f3802ffdf3f85d318af8e4bfdcc2b81
Nishant Garg <nishantgarg76342@gmail.com>
2020-06-06 00:25:34 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text(
+                "Chart Text",
+                textAlign: TextAlign.center,
+              ),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Container(
+              child: Text(
+                "List of Texts",
+              ),
+            ),
+            elevation: 5,
+          )
+        ],
+      ),
+    );
+  }
+}
