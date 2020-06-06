b6cc2cf336bc0edcd2434ac54e66ae440e9a89a7
Nishant Garg <nishantgarg76342@gmail.com>
2020-06-06 12:43:41 +0530

    + Transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter App',
+    return MaterialApp(title: 'Flutter App',
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> trans = [
+    Transaction(id: "t1", title: "New Shoes",amount: 19.99, description: "Bought new Shoes", timeStamp: DateTime.now()),
+    Transaction(id: "t2", title: "New Phone",amount: 199.9, description: "Bought new Gadget", timeStamp: DateTime.now()),
+    Transaction(id: "t3", title: "New Laptop",amount: 9.99, description: "Bought new Gadget", timeStamp: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
