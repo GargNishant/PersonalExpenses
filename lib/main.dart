2a9f5aeb9305172e2085716edcca3db8f1122924
Nishant Garg <nishantgarg76342@gmail.com>
2020-06-07 14:20:59 +0530

    *(AddTransaction) Moved as Bottom Sheet - userTransaction.dart *(main.dart) States Lifted to here

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:personalexpenses/widgets/userTransaction.dart';
+import 'package:flutter/widgets.dart';
+import 'package:personalexpenses/models/transaction.dart';
+import 'package:personalexpenses/widgets/addTransactionWidget.dart';
+import 'package:personalexpenses/widgets/transactionList.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,15 +16,48 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _transactions = [
+    Transaction(
+        id: "t1",
+        title: "New Shoes",
+        amount: 19.99,
+        description: "Bought new Shoes",
+        timeStamp: DateTime.now()),
+    Transaction(
+        id: "t2",
+        title: "New Phone",
+        amount: 199.9,
+        description: "Bought new Gadget",
+        timeStamp: DateTime.now()),
+    Transaction(
+        id: "t3",
+        title: "New Laptop",
+        amount: 9.99,
+        description: "Bought new Gadget",
+        timeStamp: DateTime.now()),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Flutter App')),
+      appBar: AppBar(
+        title: Text('Flutter App'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _startNewTransaction(context),
+          )
+        ],
+      ),
       //
-      body: SingleChildScrollView( // When the Softkeyboard Comes up, as the Screen has not much space and without scrollable, many items are hidden, that's why we added Scroll View
-        child: Column( //here to make it scrollable in area, situation, where display size is too small to fit
+      body: SingleChildScrollView(
+        child: Column(
           children: <Widget>[
             Container(
               child: Container(
@@ -36,10 +72,42 @@ class MyHomePage extends StatelessWidget {
                 ),
               ),
             ),
-            UserTransaction(),
+            TransactionList(_transactions),
           ],
         ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _startNewTransaction(context),
+      ),
     );
+  }
+
+  void _startNewTransaction(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          behavior: HitTestBehavior.opaque,
+          child: AddTransaction(_addNewTransaction),
+
+        );
+      },
+    );
+  }
+
+  void _addNewTransaction({String title, double amount}) {
+    final newTx = Transaction(
+        title: title,
+        amount: amount,
+        timeStamp: DateTime.now(),
+        description: "Newly Added",
+        id: DateTime.now().toString());
+
+    setState(() {
+      _transactions.add(newTx);
+    });
   }
 }
--- lib/widgets/addTransactionWidget.dart
+++ lib/widgets/addTransactionWidget.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
-class AddTransaction extends StatelessWidget {
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+class AddTransaction extends StatefulWidget {
   final Function function;
 
   AddTransaction(this.function);
+
+  @override
+  _AddTransactionState createState() => _AddTransactionState();
+}
+
+class _AddTransactionState extends State<AddTransaction> {
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -39,12 +45,12 @@ class AddTransaction extends StatelessWidget {
     );
   }
 
-  // Using Anonymous Function here, as this Widget contains the Params which has to be passed. Unlike FlutterGuide, which has params that needs to be passed in diff class
   void _submitData() {
-    if (titleController.text.isEmpty ||
-        double.tryParse(amountController.text) <= 0) return;
-    function(
+    if (titleController.text.isEmpty || double.tryParse(amountController.text) <= 0)
+      return;
+    widget.function(
         title: titleController.text,
         amount: double.tryParse(amountController.text));
+    Navigator.of(context).pop();
   }
 }
