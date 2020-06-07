6f69161b71de781eb7184f720e1c68b588de41bb
Nishant Garg <nishantgarg76342@gmail.com>
2020-06-07 11:20:40 +0530

    +(main.dart) Scrollable page for Smaller Screens and Keyboard popups

--- lib/main.dart
+++ lib/main.dart
@@ -19,23 +19,26 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Flutter App')),
-      body: Column(
-        children: <Widget>[
-          Container(
-            child: Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.amber,
-                elevation: 5,
-                child: Text(
-                  "Chart Text",
-                  textAlign: TextAlign.center,
+      //
+      body: SingleChildScrollView( // When the Softkeyboard Comes up, as the Screen has not much space and without scrollable, many items are hidden, that's why we added Scroll View
+        child: Column( //here to make it scrollable in area, situation, where display size is too small to fit
+          children: <Widget>[
+            Container(
+              child: Container(
+                width: double.infinity,
+                child: Card(
+                  color: Colors.amber,
+                  elevation: 5,
+                  child: Text(
+                    "Chart Text",
+                    textAlign: TextAlign.center,
+                  ),
                 ),
               ),
             ),
-          ),
-          UserTransaction(),
-        ],
+            UserTransaction(),
+          ],
+        ),
       ),
     );
   }
