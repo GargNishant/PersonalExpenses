4c7d914256dd9a8527ab416f985dcf440e49e744
Nishant Garg <nishantgarg76342@gmail.com>
2020-06-09 21:56:25 +0530

    *(chart.dart) Aligning Charts

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -10,7 +10,7 @@ class ChartWidget extends StatelessWidget {
 
   List<Map<String, Object>> get groupTransactionsValues {
     return List.generate(7, (index) {
-      final weekDay = DateTime.now().subtract(Duration(days: (6-index)));
+      final weekDay = DateTime.now().subtract(Duration(days: (6 - index)));
 
       var weekDate = DateFormat("yyyy-MM-dd").format(weekDay);
       var totalSum = 0.0;
@@ -18,8 +18,7 @@ class ChartWidget extends StatelessWidget {
       for (var i = 0; i < recentTransaction.length; i++) {
         Transaction item = recentTransaction[i];
         var itemDate = DateFormat("yyyy-MM-dd").format(item.timeStamp);
-        if (itemDate == weekDate)
-          totalSum += item.amount;
+        if (itemDate == weekDate) totalSum += item.amount;
       }
 
       return {
