15c234ce2cedc7766e611c92ec77a88e116cb662
Nishant Garg <nishantgarg76342@gmail.com>
2020-06-09 21:52:46 +0530

    *(transactionList.dart) Using LayoutBuilder for responsive

--- lib/widgets/transactionList.dart
+++ lib/widgets/transactionList.dart
@@ -1,32 +1,40 @@
-
 import 'package:flutter/material.dart';
 import 'package:personalexpenses/models/transaction.dart';
 import 'package:personalexpenses/widgets/transactionWidget.dart';
 
-class TransactionList extends StatelessWidget{
+class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
   final Function _deleteFunction;
 
-  TransactionList(this.transactions,this._deleteFunction);
+  TransactionList(this.transactions, this._deleteFunction);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      child: transactions.isEmpty ?
-        Column(children: <Widget>[
-          Text("Waiting for 1st Transactions", style: Theme.of(context).textTheme.headline6,),
-          SizedBox(height: 20,),
-          Container(height: 250,child: Image.asset("assets/images/waiting.png",fit: BoxFit.cover,)),
-        ],)
-        :
-        ListView.builder(
-        itemBuilder: (ctX,index){
-          return TransWidget(transactions[index],_deleteFunction);
-        },
-        itemCount: transactions.length,
-      ),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return transactions.isEmpty
+            ? Column(
+                children: <Widget>[
+                  Text(
+                    "Waiting for 1st Transactions",
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  SizedBox(height: constraints.maxHeight * 0.05),
+                  Container(
+                      height: constraints.maxHeight * 0.5,
+                      child: Image.asset(
+                        "assets/images/waiting.png",
+                        fit: BoxFit.cover,
+                      )),
+                ],
+              )
+            : ListView.builder(
+                itemBuilder: (ctX, index) {
+                  return TransWidget(transactions[index], _deleteFunction);
+                },
+                itemCount: transactions.length,
+              );
+      },
     );
   }
-
 }
